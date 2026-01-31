0e11aa03b19ebe6f0e5ebc5ad47ca4ac1e5dfa00
rnlisboa <dev.renanlisboa@gmail.com>
2026-01-30 23:49:37 -0300

    fix: remove session

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -1,11 +1,10 @@
-import { User, Session } from 'generated/prisma/client';
+import { User } from 'generated/prisma/client';
 
 export class UserEntity implements User {
   id: string;
   name: string;
   email: string;
   password: string;
-  sessions?: Session[];
   createdAt: Date;
   updatedAt: Date;
 }
